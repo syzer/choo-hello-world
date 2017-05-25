fa6ebfa8135847f0523b60cbfd7050978eb0f27a
syzer3@gmail.com <syzer3@gmail.com>
2017-05-25 16:18:09 +0200

    adding swal

--- template/main.js
+++ template/main.js
@@ -1,13 +1,23 @@
 // import choo's template helper
 const html = require('choo/html')
 const animal = require('./animal.js')
-
+const swal = require('sweetalert2')
 
 // export module
 module.exports = function (state) {
   const type = state.animals.type
   const x = state.animals.x
   const y = state.animals.y
+
+  swal({
+    title: 'Just a WIP',
+    html: $('<div>')
+    .addClass('some-class')
+    .text('jQuery is everywhere.'),
+    animation: false,
+    customClass: 'animated tada'
+  })
+
 
   // create html template
   return html`
@@ -18,4 +28,13 @@ module.exports = function (state) {
       </div>
     </div>
   `
+
+  // add new animal to state
+  function add (event) {
+    const x = event.offsetX - 20
+    const y = event.offsetY - 10
+
+    const obj = { x: x, y: y }
+    emit('addAnimal', obj)
+  }
 }
